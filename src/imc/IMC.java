fac02f851701fa983561f04d2e68ac45cf2b6cd5
Gabriel Júnior Griseli <gabrielgriseli@gmail.com>
2016-08-11 22:12:21 -0300

    Salvando no GitHub2

--- src/imc/IMC.java
+++ src/imc/IMC.java
@@ -1,11 +1,12 @@
 package imc;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class IMC {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         
@@ -14,8 +15,12 @@ public class IMC {
         
         System.out.println("Digite o sexo da pessoa: ");
         sexo = in.readLine();
+        System.out.println("Digite a altura da pessoa (use vírgula para números fracionados): ");
+        altura = Double.parseDouble(in.readLine());
+        System.out.println("Digite a idade da pessoa (use vírgula para números fracionados): ");
+        peso = Double.parseDouble(in.readLine());
         
-        
+        CalculaIMC(sexo, altura, peso);
         
     }
     
